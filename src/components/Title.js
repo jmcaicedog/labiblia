17e13620801893c139e997a5883fbad496d3d996
Juan Maenuel Caicedo <juanmanuel@microingenieria.net>
2023-12-27 01:12:29 -0500

    Title color changed

---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Title = ({ title }) => {
+  return (
+    <div className="text-center text-4xl font-bold pb-5 text-cafe">{title}</div>
+  );
+};
+
+export default Title;
